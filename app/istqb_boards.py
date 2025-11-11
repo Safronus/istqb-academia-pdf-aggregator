30e853af833cf3f0e1543420aafe4956bdee6c60
Safronus <safronus@gmail.com>
2025-11-11 23:08:28 +0100

    Board Codes

--- app/istqb_boards.py
+++ app/istqb_boards.py
@@ -4,40 +4,75 @@ from __future__ import annotations
 # Unknown names are allowed; this set is for hinting only.
 KNOWN_BOARDS = {
     # EMEA / Europe
-    "CaSQB",  # Czech and Slovak Quality Board (renamed from CaSTB)
-    "GTB",    # German Testing Board
-    "STB",    # Swiss Testing Board
-    "UKITB",  # UK and Ireland Testing Board
-    "BNTQB",  # Belgium and Netherlands Testing Qualifications Board
-    "ATB",    # Austrian Testing Board
-    "SEETB",  # South East European Testing Board
-    "SSTQB",  # Spanish Software Testing Qualifications Board
-    "CFTL",   # Comité Français des Tests Logiciels
-    "PSTB",   # Polish Software Testing Board
-    "ISTB",   # Italian Software Testing Board
-    "DSTB",   # Danish Software Testing Board
-    "FiSTB",  # Finnish Software Testing Board
-    "NQTB",   # Norwegian
-    "SASTQB", # South African Software Testing Qualifications Board
-    "HASTQB", # Hispanoamerican
-    "LSTQB",  # Latvian
-    "LTB",    # Lithuanian Testing Board
-    "GRTB",   # Greek
-    "PTQB",   # Portuguese
-    "TMMi",   # sometimes used in folder naming alongside boards
-
+    "CaSQB",   # Czech and Slovak Quality Board
+    "GTB",     # German Testing Board
+    "STB",     # Swiss Testing Board
+    "UKITB",   # UK and Ireland Testing Board
+    "BNTQB",   # Belgium and Netherlands Testing Qualifications Board
+    "ATB",     # Austrian Testing Board
+    "SEETB",   # South East European Testing Board
+    "SSTQB",   # Spanish Software Testing Qualifications Board
+    "CFTL",    # Comité Français des Tests Logiciels (France)
+    "PSTB",    # Polish Software Testing Board
+    "ISTB",    # Italian Software Testing Board
+    "DSTB",    # Danish Software Testing Board
+    "FiSTB",   # Finnish Software Testing Board
+    "NSTB",    # Norwegian Software Testing Board
+    "SSTB",    # Swedish Software Testing Board
+    "SASTQB",  # South African Software Testing Qualifications Board
+    "HASTQB",  # Hispanoamerican Software Testing Qualifications Board
+    "LSTQB",   # Latvian Software Testing Qualification Board
+    "LTB",     # Lithuanian Testing Board
+    "HTB",     # Hellenic Testing Board (Greece)
+    "PSTQB",   # Portuguese Software Testing and Qualifications Board
+    "RTB",     # Romanian Testing Board
+    "SBTB",    # Serbian Testing Board
+    "HSTB",    # Hungarian Software Testing Board
+    "USQB",    # Ukrainian Software Quality Board
+    "RSTQB",   # Russian Software Testing Qualifications Board
+    "TTB",     # Turkish Testing Board
+    "ESTB",    # Egyptian Software Testing Board
+    "NGSTQB",  # Nigerian Software Testing and Qualifications Board
+    "KSATB",   # Saudi Arabia Testing Board
+    "ISTB-IL", # Israeli Software Testing Board
+    "JTB",     # Jordanian Testing Board
+    "LTB-LB",  # Lebanese Testing Board
+    "UAESTQB", # UAE Software Testing Qualifications Board
+    "TSQB",    # Tunisian Software Qualifications Board
+    "ISTQB-IR",# Iranian Software Testing Qualifications Board
+    
     # Americas
-    "ASTQB",  # American Software Testing Qualifications Board
-    "BSTQB",  # Brazilian
-    "CSTB",   # Canadian
-
-    # APAC / Others
-    "ANZTB",  # Australian and New Zealand Testing Board
-    "MSTB",   # Malaysia Software Testing Board
-    "SLSTB",  # Sri Lanka Software Testing Board
-    "INDSTB", # India (some orgs use this code)
-    "HSTQB",  # Hong Kong
-    "JSTQB",  # Japan
-    "SWTB",   # Sweden (legacy/alt naming)
-    "NGSTQB", # Nigeria (EMEA)
+    "ASTQB",   # American Software Testing Qualifications Board (USA)
+    "BSTQB",   # Brazilian Software Testing Qualifications Board
+    "CSTB",    # Canadian Software Testing Board
+    "MTB",     # Mexican Testing Board
+    "ATB-ARG", # Argentine Testing Board
+    "CoSTQB",  # Colombian Software Testing and Qualifications Board
+    "PTB-PE",  # Peruvian Testing Board
+    "UTB",     # Uruguayan Testing Board
+    "VSTQB",   # Venezuelan Software Testing Qualifications Board
+    "CTB-CR",  # Caribbean Testing Board
+    "CuTB",    # Cuban Software Testing Board
+    
+    # APAC / Asia-Pacific
+    "ANZTB",   # Australian and New Zealand Testing Board
+    "MSTB",    # Malaysia Software Testing Board
+    "SLSTB",   # Sri Lanka Software Testing Board
+    "ITB",     # Indian Testing Board
+    "HKSTQB",  # Hong Kong Software Testing Qualifications Board
+    "JSTQB",   # Japan Software Testing Qualifications Board
+    "KSTQB",   # Korean Software Testing Qualifications Board
+    "CSTQB",   # China Software Testing Qualifications Board
+    "TW-STQB", # Taiwan Software Testing Qualifications Board
+    "SG-STB",  # Singapore Software Testing Board
+    "TH-STB",  # Thai Software Testing Board
+    "VTB",     # Vietnamese Testing Board
+    "PhilSTQB",# Philippine Software Testing Qualifications Board
+    "INA-STQB",# Indonesian Software Testing Qualifications Board
+    "PSTQB-PK",# Pakistan Software Testing Qualifications Board
+    "BSTB-BD", # Bangladesh Software Testing Board
+    "ZimSTQB", # Zimbabwe Software Testing Qualification Board
+    
+    # Other/Legacy codes
+    "TMMi",    # TMMi Foundation (sometimes used in folder naming)
 }
